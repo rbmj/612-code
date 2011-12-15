7c4b6012240284464f7aa93f7e8d3b7183d62456
rbmj <rbmj@verizon.net>
2011-12-15 12:28:52 -0500

    Updated jag reversal so the robot drives straight!

--- ports.cpp
+++ ports.cpp
@@ -4,10 +4,10 @@
 
 
 //PWMs                       SLOT   PORT
-Jaguar left_front_jag       ( 4,     5 );
 Jaguar left_rear_jag        ( 4,     2 );
-Jaguar right_front_jag      ( 4,     4 );
 Jaguar right_rear_jag       ( 4,     3 );
+Jaguar right_front_jag      ( 4,     4 );
+Jaguar left_front_jag       ( 4,     5 );
 
 //DIOs
 
@@ -20,7 +20,7 @@ Joystick            left_joystick ( 1  );
 Joystick            right_joystick( 2  );
 
 //drive_jaguar                           JAGUAR&                 TYPE               REVERSE
-drive_jaguar left_front_motor =     { left_front_jag,  RobotDrive::kFrontLeftMotor,  true  };
-drive_jaguar left_rear_motor =      { left_rear_jag,   RobotDrive::kRearLeftMotor,   true  };
+drive_jaguar left_front_motor =     { left_front_jag,  RobotDrive::kFrontLeftMotor,  false };
+drive_jaguar left_rear_motor =      { left_rear_jag,   RobotDrive::kRearLeftMotor,   false };
 drive_jaguar right_front_motor =    { right_front_jag, RobotDrive::kFrontRightMotor, false };
 drive_jaguar right_rear_motor =     { right_rear_jag,  RobotDrive::kRearRightMotor,  false };
