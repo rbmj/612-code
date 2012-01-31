395009475ba46acf5b3aef93bf14a24743366957
Robert Blair Mason Jr. <rbmj@verizon.net>
2012-01-31 10:45:48 -0500

    update ports to reflect new module addressing scheme.

--- ports.cpp
+++ ports.cpp
@@ -35,19 +35,30 @@
 
 //just define & initialize all of the consts in ports.h
 
+//slots table - in order to make sync with electrical easier.
+const module_t slot1 = 1; //1st analog
+const module_t slot2 = 1; //1st digital
+const module_t slot3 = 1; //1st solenoid
+const module_t slot4 = 0; //EMPTY
+const module_t slot5 = 2; //2nd analog
+const module_t slot6 = 2; //2nd digital
+const module_t slot7 = 2; //2nd solenoid
+const module_t slot8 = 0; //EMPTY
+
 //PORTS TABLE
 
-//PWMs                                  SLOT   PORT
-Jaguar right_front_jag                  ( 4,     3 );
-Jaguar right_rear_jag                   ( 4,     4 );
-Jaguar left_front_jag                   ( 6,     1 );
-Jaguar left_rear_jag                    ( 6,     2 );
-Jaguar right_launcher_jag               ( 6,     3 );
-Jaguar left_launcher_jag                ( 6,     4 );
-Jaguar turret_XY_control_jag            ( 4,     1 );
-Jaguar turret_Z_control_jag             ( 4,     2 );
-Servo right_servo_shift                 ( 4,     5 );
-Servo left_servo_shift                  ( 6,     5 );
+//PWMs                                     SLOT    PORT
+Jaguar turret_XY_control_jag            ( slot2,     1 );
+Jaguar turret_Z_control_jag             ( slot2,     2 );
+Jaguar right_front_jag                  ( slot2,     3 );
+Jaguar right_rear_jag                   ( slot2,     4 );
+Servo right_servo_shift                 ( slot2,     5 );
+
+Jaguar left_front_jag                   ( slot6,     1 );
+Jaguar left_rear_jag                    ( slot6,     2 );
+Jaguar right_launcher_jag               ( slot6,     3 );
+Jaguar left_launcher_jag                ( slot6,     4 );
+Servo left_servo_shift                  ( slot6,     5 );
 
 //NOTE: Sica wants to use pots (potentiometers) for the angles, as those are
 //absolute and don't need a zero switch.  If we do that they're seen as
@@ -55,33 +66,35 @@ Servo left_servo_shift                  ( 6,     5 );
 //an angular offset, but we'll cross that bridge when we come to it.
 
 //note: for two channel encoders, we need to specify a slot for both ports
-//DIOs                                  SLOT   PORT
-Encoder right_drive                     ( 4,     1,
-                                          4,     2 );
-Encoder left_drive                      ( 4,     3,
-                                          4,     4 );
-Encoder launcher_wheel                  ( 4,     5,
-                                          4,     6 );
-Encoder launch_angle                    ( 4,     7,
-                                          4,     8 );
+//DIOs                                    SLOT     PORT
+Encoder right_drive                     ( slot2,     1,
+                                          slot2,     2 );
+Encoder left_drive                      ( slot2,     3,
+                                          slot2,     4 );
+Encoder launcher_wheel                  ( slot2,     5,
+                                          slot2,     6 );
+Encoder launch_angle                    ( slot2,     7,
+                                          slot2,     8 );
 
-DigitalInput launch_angle_switch        ( 6,     1 );
-Ultrasonic front_ultrasonic             ( 6,     2,
-                                          6,     3 );
+DigitalInput launch_angle_switch        ( slot6,     1 );
+Ultrasonic front_ultrasonic             ( slot6,     2,
+                                          slot6,     3 );
                                           
 //note: since we rely on the default value of kInches for the 5th arg
 //we should use Ultrasonic::GetRangeInches().
 
-//AIOs                                  SLOT   PORT
+//AIOs                                    SLOT     PORT
+AnalogChannel launch_angle_pot          ( slot1,     1 );
 
-//Relays                                SLOT   PORT
-Relay roller_spike_1                    ( 4,     1 );
-Relay roller_spike_2                    ( 4,     2 );
+//Relays                                  SLOT     PORT
+Relay roller_spike_1                    ( slot4,     1 );
+Relay roller_spike_2                    ( slot4,     2 );
 
 //USBs (on driver station)                     PORT
 Joystick left_joystick_raw                     ( 1 );
 Joystick right_joystick_raw                    ( 2 );
 Joystick gunner_joystick_raw                   ( 3 );
+
 
 joysmooth left_joystick        ( left_joystick_raw );
 joysmooth right_joystick      ( right_joystick_raw );
@@ -91,7 +104,7 @@ joysmooth gunner_joystick    ( gunner_joystick_raw );
 
 //RobotDrive
 RobotDrive drive (
-    left_front_motor.jag,  //KEEP THIS ORDER!  IT'S IMPORTANT!
+    left_front_motor.jag,  //KEEP THIS ORDER (HERE)!  IT'S IMPORTANT!
     left_rear_motor.jag,   //LF, LR, RF, RR -- see documentation
     right_front_motor.jag, //for more details
     right_rear_motor.jag
