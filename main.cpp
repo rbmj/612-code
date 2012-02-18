cdd0e4681a1fd402bdb178e49e90c6595c2d2921
unknown <Mikice@Mikice-PC.(none)>
2012-02-18 11:24:40 -0500

    Moved states out of Main into own files.

--- main.cpp
+++ main.cpp
@@ -27,6 +27,9 @@
 #include "state_tracker.h"
 #include "visionalg.h"
 #include "shifter.h"
+#include "Shooting_Manual.h"
+#include "Shooting_Auto.h"
+#Include "Shooting_Driving.h"
 
 //constructor - initialize drive
 robot_class::robot_class() {
@@ -78,97 +81,14 @@ void robot_class::AutonomousContinuous() {
 }
 
 void robot_class::TeleopContinuous() {
-    if(global_state.get_state() == STATE_DRIVING) {
-        //Turret rotation controlled by gunner joystick during drive state only. Must press button 1
-        if (gunner_joystick.GetRawButton(1)) {
-            turret_rotation_jag.Set(gunner_joystick.GetX());
-        }
-        if (left_joystick.GetRawButton(1)) {
-            //arcade drive
-            drive.ArcadeDrive(left_joystick); //arcade drive on left joystick
-        }
-        else {
-            //tank drive
-            float left = left_joystick.GetY();
-            float right = right_joystick.GetY();
-            //explicitly state drive power is based on Y axis of that side joy
-            drive.TankDrive(left, right);
-        }
-        if (right_joystick.GetRawButton(11) || left_joystick.GetRawButton(11)) {
-            servo_shifter.set(shifter::HIGH);
-            // set servo to high gear
-        }
-        else if (left_joystick.GetRawButton(10) || right_joystick.GetRawButton(10)) {
-            servo_shifter.set(shifter::LOW);
-            //Sets servo to low gear
-        }
-        if(left_joystick.GetRawButton(3)) {
-            global_state.set_state(STATE_SHOOTING);
-        }
-        //Turret rotation controlled by gunner joystick during drive state only. Must press button 1
-        if(gunner_joystick.GetRawButton(1){
-            turret_rotation_jag.Set(gunner_joystick.GetX());
-        }
-        if(bridge_arm_switch.get()!=1){//limit switch not pressed
-            bridge_arm_spike.set(kOn);
-            if(gunner_joystick.GetRawButton(2){//up
-               bridge_arm_spike.set(kForwardOnly);
-            }
-            if(gunner_joystick.GetRawButton(3){//down
-               bridge_arm_spike.set(kReverseOnly);
-            }
-        }
-        bridge_arm_spike.set(kOff);
-    }
-    else if(global_state.get_state() == STATE_SHOOTING) {
-        // disable motor safety check to stop wasting netconsole space
-        drive.SetSafetyEnabled(false);
-        vision_processing::update();
-        vector<double> target_degrees = vision_processing::get_degrees();
-        vector<double> target_distances = vision_processing::get_distance();
-        printf("Number of targets detected: %d\n", target_degrees.size());
-        if(target_degrees.size() >= 1) {
-            printf("Angle (degrees) of camera: %f\n", target_degrees[0]);
-        }
-        else {
-            printf("No target detected\n");
-        }
-        if(target_distances.size() >= 1) {
-            printf("Distance of target:       %f\n", target_distances[0]);
-        }
-        if(!left_joystick.GetRawButton(3)) {
-            global_state.set_state(STATE_DRIVING);
-            drive.SetSafetyEnabled(true);
-        }
-    }
-    //TEMPORARY: GUNNER LAUNCHER WHEEL CONTROLS
-    if (gunner_joystick.GetRawButton(7)) {
-        left_launcher_jag.Set(1.0);
-        right_launcher_jag.Set(1.0);
-    }
-    else {
-        left_launcher_jag.Set(0.0);
-        right_launcher_jag.Set(0.0);
-    }
-    if (global_state.get_state() != STATE_SHOOTING) {
-        //MANUAL ROLLER CONTROL
-        if (gunner_joystick.GetRawButton(11)) {
-            //rollers up
-            rollers.set_direction(roller_t::UP);
-        }
-        else if (gunner_joystick.GetRawButton(10)) {
-            //rollers down
-            rollers.set_direction(roller_t::DOWN);
-        }
-        else if (gunner_joystick.GetRawButton(9)) {
-            rollers.set_direction(roller_t::OFF);
-        }
-        else {
-            //auto belts
-        }
-        //
-        Wait(0.005); //let the CPU rest a little - 5 ms isn't too long
-    }
+    shooting_manual();
+    // Calls Manual Shooting
+    shooting_auto();
+    // Calls Auto Shooting
+    shooting_driving();
+    // Calls Driving Shooting
+    Wait(0.005); //let the CPU rest a little - 5 ms isn't too long
+
 }
 
 void robot_class::update_sensors() {
