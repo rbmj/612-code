4d5b7399d7b25a2876aedb31ebeb5dfdcef1ac52
anidev <anidev.aelico@gmail.com>
2012-02-21 17:32:17 -0500

    winch jag wrapper to use limit switch (when we were using pid)

---
+++ winch_motor.h
@@ -0,0 +1,38 @@
+/* winch_motor.h
+ *
+ * Copyright (c) 2011, 2012 Chantilly Robotics <chantilly612@gmail.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/*
+ * Wrapper class for winch jag to incoporate limit switch
+ */
+
+#ifndef INC_WINCH_MOTOR_H
+#define INC_WINCH_MOTOR_H
+
+#include <Jaguar.h>
+#include <DigitalInput.h>
+#include <PIDOutput.h>
+
+class winch_motor : public PIDOutput {
+private:
+    Jaguar* jag;
+    DigitalInput* limit_switch;
+public:
+    winch_motor(Jaguar&, DigitalInput&);
+    void PIDWrite(float);
+};
+
+#endif // INC_WINCH_MOTOR_H
